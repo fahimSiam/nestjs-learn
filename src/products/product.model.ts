fd73f032e3fb5a7c40cbd8c9186fb9c89c33e432
fahimSiam <siamfahim02@gmail.com>
2022-05-15 18:45:31 +0600

    a

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,13 +1,13 @@
-export class Product{
-    constructor(
-        public id: string,
-        public title: string,  
-        public description: string,
-        public price: number,
-        /* public image: string,
+/* eslint-disable prettier/prettier */
+export class Product {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public price: number /* public image: string,
         public category: string,
         public rating: number,
         public created: Date,
-        public updated: Date, */
-    ){}
+        public updated: Date, */,
+  ) {}
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,44 +1,57 @@
-import { Controller, Post,Get, Delete,Body, Param, Patch } from "@nestjs/common";
-import { ProductsService } from "./products.service";
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Body,
+  Param,
+  Patch,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productsService: ProductsService) {}
-    
-    @Post()
-    addProduct(
-        @Body('title') prodTitle:string,
-        @Body('description') prodDecs:string,
-        @Body('price') prodPrice:number,
-    ):any {
-       const generatedId= this.productsService.insertProduct(prodTitle, prodDecs, prodPrice);
-         return {id: generatedId};
-    }
+  constructor(private readonly productsService: ProductsService) {}
 
-    @Get()
-    getAllProducts(){
-        return this.productsService.getProducts();
-    }
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDecs: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const generatedId = this.productsService.insertProduct(
+      prodTitle,
+      prodDecs,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
 
-    @Get(':id')
-    getProduct(@Param('id') prodId:string){
-        return this.productsService.getSingleProduct(prodId);
-    }
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
 
-    @Patch(':id')
-    updateProduct(
-        @Param('id') prodId:string,
-        @Body('title') prodTitle:string,
-        @Body('description') prodDecs:string,
-        @Body('price') prodPrice:number,
-    ){
-        this.productsService.updateProducts(prodId,prodTitle,prodDecs,prodPrice);
-        return null;
-    }
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId);
+  }
 
-    @Delete(':id')
-    removeProduct(@Param('id') prodId:string){
-        this.productsService.removeProduct(prodId);
-        return {'msg': 'Product deleted'};
-    }
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDecs: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProducts(prodId, prodTitle, prodDecs, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string) {
+    this.productsService.removeProduct(prodId);
+    return { msg: 'Product deleted' };
+  }
 }

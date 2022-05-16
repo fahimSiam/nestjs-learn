1bdf0190472bcf8f5390b9e7a4d5e9f5661320d8
fahimSiam <siamfahim02@gmail.com>
2022-05-16 10:39:27 +0600

    prettier fixes

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { ProductsController } from "./products.controller";
-import { ProductsService } from "./products.service";
+import { Module } from '@nestjs/common';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 @Module({
-    controllers: [ProductsController],
-    providers: [ProductsService],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class ProductsModule {}
